a8c2ce4f4d1499e33f0a46ccfefd54638c222b99
purno <pg.purnoghosh@gmail.com>
2025-03-07 19:11:52 +0600

    new 6.3

--- src/test/java/Setup_All/Setup.java
+++ src/test/java/Setup_All/Setup.java
@@ -41,8 +41,10 @@ public class Setup {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080"); // Set a specific window size
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
 //        ChromeOptions options = new ChromeOptions();
 //        driver = new ChromeDriver(options);
